50d4468260513250ab3a4cfaf325b6e36f8c32ea
mixed2004 <mixed2004@mail.ru>
2023-07-18 22:17:51 +0300

    4.1.2

---
+++ config-client/src/main/java/web/ConfigClient.java
@@ -0,0 +1,15 @@
+package web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClient {
+    public static void main(String[] args) {
+
+        SpringApplication.run(ConfigClient.class, args);
+
+    }
+}
